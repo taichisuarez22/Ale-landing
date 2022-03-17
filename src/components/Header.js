a5cf3e01ded324860feb1a4f81ed107d10cdb57a
alejandro taichi suarez <alejandrotaichisuarezuy@gmail.com>
2022-03-16 23:38:03 -0300

    se rehacen cambios borrados. Header actua como Navbar. se cambia el fondo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,25 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+
 
 export default function Header() {
 
     return(
     <div>
-            <h1> header</h1>
-            <Button variant="primary">Primary</Button>
+            <h1> Bienvenidos</h1>
+            <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
     </div>
     );
     }
